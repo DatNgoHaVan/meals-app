b0fcb2e7224c39f1f779ae3b5d2a048ecba5419e
datngo <ngohavandat93ndc@gmail.com>
2020-07-31 00:32:45 +0700

    Update Meal Item Detail

--- screens/CategoryMealsScreen.tsx
+++ screens/CategoryMealsScreen.tsx
@@ -2,16 +2,29 @@ import React from 'react'
 import { StyleSheet, Text, View, Button, Platform, FlatList } from 'react-native'
 import { CATEGORIES, MEALS } from '../data/dummy-data';
 import Colors from '../constants/Colors';
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = (props: any) => {
   const catId = props.navigation.getParam('categoryId');
 
-  const selectedCat = CATEGORIES.find(cat => cat.id === catId);
-
   const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
 
   const renderMealItem = (itemData: any) => (
-    <View><Text>{itemData.item.title}</Text></View>
+    <MealItem
+      title={itemData.item.title}
+      duration={itemData.item.duration}
+      complexity={itemData.item.complexity}
+      affordability={itemData.item.affordability}
+      image={itemData.item.imageUrl}
+      onSelectMeal={() => {
+        props.navigation.navigate({
+          routeName: 'MealDetail',
+          params: {
+            mealId: itemData.item.id
+          }
+        })
+      }}
+    />
   );
 
   return (
@@ -20,6 +33,7 @@ const CategoryMealsScreen = (props: any) => {
         data={displayedMeals}
         keyExtractor={(item, index) => item.id}
         renderItem={renderMealItem}
+        style={{ width: '100%' }}
       />
     </View>
   )
@@ -41,6 +55,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 15
   }
 })
--- screens/MealDetailScreen.tsx
+++ screens/MealDetailScreen.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
+import { MEALS } from '../data/dummy-data';
 
 const MealDetailScreen = (props: any) => {
+  const mealId = props.navigation.getParam('mealId');
+
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
   return (
     <View style={styles.screen}>
       <Text>
-        Meal detail screens!
+        {selectedMeal?.title}
       </Text>
       <Button title="Go back to Categories" onPress={() => {
         props.navigation.popToTop();
@@ -14,7 +19,15 @@ const MealDetailScreen = (props: any) => {
   )
 }
 
-export default MealDetailScreen
+MealDetailScreen.navigationOptions = (navigationData: any) => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal?.title
+  };
+};
+
+export default MealDetailScreen;
 
 const styles = StyleSheet.create({
   screen: {
