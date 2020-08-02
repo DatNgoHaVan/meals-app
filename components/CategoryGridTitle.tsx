557d932ea01069a8a41488a2cfcbf638ab533fb8
datngo <ngohavandat93ndc@gmail.com>
2020-08-02 15:27:56 +0700

    Fix Grid Shadow on Android

--- components/CategoryGridTitle.tsx
+++ components/CategoryGridTitle.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Platform, TouchableNativeFeedback } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Platform, TouchableNativeFeedback } from 'react-native';
 
 const CategoryGridTitle = (props: any) => {
   let TouchableCmp: any = TouchableOpacity;
@@ -28,7 +28,8 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: 'hidden'
+    overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden' : 'visible',
+    elevation: 5,
   },
   container: {
     flex: 1,
@@ -37,7 +38,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end'
