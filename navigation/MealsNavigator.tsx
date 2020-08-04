a8a058506bc8c7124377a4111f0629a1eb5eb581
datngo <ngohavandat93ndc@gmail.com>
2020-08-04 22:38:08 +0700

    Update Navigation

--- navigation/MealsNavigator.tsx
+++ navigation/MealsNavigator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createAppContainer, createStackNavigator, createBottomTabNavigator, createDrawerNavigator } from 'react-navigation';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import { Ionicons } from '@expo/vector-icons';
@@ -15,6 +15,12 @@ import FiltersScreen from '../screens/FiltersScreen';
 const defaultStackNavOptions = {
   headerStyle: {
     backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+  },
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold',
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans',
   },
   headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
   headerTitle: 'A Screen'
@@ -44,17 +50,23 @@ const tabScreenConfig = {
       tabBarIcon: (tabInfo: any) => (
         <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />
       ),
-      tabBarColor: Colors.primaryColor
+      tabBarColor: Colors.primaryColor,
+      tabBarLabel: Platform.OS === 'android'
+        ? <Text style={{ fontFamily: 'open-sans-bold' }}>Meals</Text>
+        : 'Meals'
     },
   },
   Favorites: {
     screen: FavNavigator,
     navigationOptions: {
-      tabBarLabel: 'Favorites!',
       tabBarIcon: (tabInfo: any) => (
         <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
       )
-    }
+    },
+    tabBarColor: Colors.accentColor,
+    tabBarLabel: Platform.OS === 'android'
+      ? <Text style={{ fontFamily: 'open-sans-bold' }}>Favorites</Text>
+      : 'Favorites'
   }
 }
 
@@ -70,7 +82,10 @@ const MealsFavTabNavigator =
     : createBottomTabNavigator(tabScreenConfig, {
       tabBarOptions: {
         inactiveTintColor: Colors.accentColor,
-        showIcon: true
+        showIcon: true,
+        labelStyle: {
+          fontFamily: 'open-sans-bold',
+        }
       }
     });
 
